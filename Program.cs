db0f77f4393dba17aa7420eaf8835d1234519fd0
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-26 16:09:21 +0100

    version finale

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program{
+
+class Even{
+
+
+static public void Main(){
+static void IfNumberIsEven(int int1){
+
+if(int1 % 2 == 0){
+    Console.WriteLine(int1  + " est un nombre pair");
+}else{
+    Console.WriteLine(int1  + " est un nombre impair");
+}
+}
+IfNumberIsEven(664);
+}
+}
+
+}
